5234631f7d2a3cbd7a9ed3dc9db4f0666b087de8
Peru Riezu Muñoz <riezumunozperu@gmail.com>
2023-11-10 19:23:44 +0100

    done

---
+++ clang-update-alternatives.sh
@@ -0,0 +1,71 @@
+update-alternatives --install \
+        /usr/bin/llvm-config           llvm-config           /usr/bin/llvm-config-13 200
+update-alternatives --install \
+        /usr/bin/llvm-ar               llvm-ar               /usr/bin/llvm-ar-13 200
+update-alternatives --install \
+        /usr/bin/llvm-as               llvm-as               /usr/bin/llvm-as-13 200
+update-alternatives --install \
+        /usr/bin/llvm-bcanalyzer       llvm-bcanalyzer       /usr/bin/llvm-bcanalyzer-13 200
+update-alternatives --install \
+        /usr/bin/llvm-cov              llvm-cov              /usr/bin/llvm-cov-13 200
+update-alternatives --install \
+        /usr/bin/llvm-diff             llvm-diff             /usr/bin/llvm-diff-13 200
+update-alternatives --install \
+        /usr/bin/llvm-dis              llvm-dis              /usr/bin/llvm-dis-13 200
+update-alternatives --install \
+        /usr/bin/llvm-dwarfdump        llvm-dwarfdump        /usr/bin/llvm-dwarfdump-13 200
+update-alternatives --install \
+        /usr/bin/llvm-extract          llvm-extract          /usr/bin/llvm-extract-13 200
+update-alternatives --install \
+        /usr/bin/llvm-link             llvm-link             /usr/bin/llvm-link-13 200
+update-alternatives --install \
+        /usr/bin/llvm-mc               llvm-mc               /usr/bin/llvm-mc-13 200
+update-alternatives --install \
+        /usr/bin/llvm-nm               llvm-nm               /usr/bin/llvm-nm-13 200
+update-alternatives --install \
+        /usr/bin/llvm-objdump          llvm-objdump          /usr/bin/llvm-objdump-13 200
+update-alternatives --install \
+        /usr/bin/llvm-ranlib           llvm-ranlib           /usr/bin/llvm-ranlib-13 200
+update-alternatives --install \
+        /usr/bin/llvm-readobj          llvm-readobj          /usr/bin/llvm-readobj-13 200
+update-alternatives --install \
+        /usr/bin/llvm-rtdyld           llvm-rtdyld           /usr/bin/llvm-rtdyld-13 200
+update-alternatives --install \
+        /usr/bin/llvm-size             llvm-size             /usr/bin/llvm-size-13 200
+update-alternatives --install \
+        /usr/bin/llvm-stress           llvm-stress           /usr/bin/llvm-stress-13 200
+update-alternatives --install \
+        /usr/bin/llvm-symbolizer       llvm-symbolizer       /usr/bin/llvm-symbolizer-13 200
+update-alternatives --install \
+        /usr/bin/llvm-tblgen           llvm-tblgen           /usr/bin/llvm-tblgen-13 200
+update-alternatives --install \
+        /usr/bin/clang                 clang                 /usr/bin/clang-13 200
+update-alternatives --install \
+        /usr/bin/clangd                clangd               /usr/bin/clangd-13 200
+update-alternatives --install \
+        /usr/bin/clang++               clang++               /usr/bin/clang++-13 200
+update-alternatives --install \
+        /usr/bin/asan_symbolize        asan_symbolize        /usr/bin/asan_symbolize-13 200
+update-alternatives --install \
+        /usr/bin/clang-cpp             clang-cpp             /usr/bin/clang-cpp-13 200
+update-alternatives --install \
+        /usr/bin/clang-format          clang-format          /usr/bin/clang-format-13 200
+update-alternatives --install \
+        /usr/bin/clang-format-diff     clang-format-diff     /usr/bin/clang-format-diff-13 200
+update-alternatives --install \
+        /usr/bin/clang-include-fixer   clang-include-fixer   /usr/bin/clang-include-fixer-13 200
+update-alternatives --install \
+        /usr/bin/clang-offload-bundler clang-offload-bundler /usr/bin/clang-offload-bundler-13 200
+update-alternatives --install \
+        /usr/bin/clang-query           clang-query           /usr/bin/clang-query-13 200
+update-alternatives --install \
+        /usr/bin/clang-rename          clang-rename          /usr/bin/clang-rename-13 200
+update-alternatives --install \
+        /usr/bin/clang-reorder-fields  clang-reorder-fields  /usr/bin/clang-reorder-fields-13 200
+update-alternatives --install \
+        /usr/bin/clang-tidy            clang-tidy            /usr/bin/clang-tidy-13 200
+update-alternatives --install \
+        /usr/bin/lldb                  lldb                  /usr/bin/lldb-13 200
+update-alternatives --install \
+        /usr/bin/lldb-server           lldb-server           /usr/bin/lldb-server-13 200
+
